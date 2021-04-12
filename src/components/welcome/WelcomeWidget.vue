3499f9c2c12b45801a95d3a272ea4d9c87753e5e
Jan Langheimer <langheimer@gmail.com>
2021-04-12 12:28:52 +0200

    add group hover

--- src/components/welcome/WelcomeWidget.vue
+++ src/components/welcome/WelcomeWidget.vue
@@ -1,14 +1,19 @@
 <template>
-  <div
-    class="w-[327px] py-8 px-5 bg-white rounded-[40px] shadow-custom hover:shadow-custom4 transition-shadow duration-500 border border-gray flex flex-col items-center"
+  <a
+    :href="props.buttonHref"
+    class="group w-[327px] py-8 px-5 bg-white rounded-[40px] shadow-custom hover:shadow-custom4 transition-shadow duration-500 border border-gray flex flex-col items-center"
   >
     <div class="w-[60px] h-[50px] flex justify-center items-center">
       <img :src="`/images/icons/${props.icon}.svg`" />
     </div>
     <h2 class="my-3">{{ props.title }}</h2>
     <p class="mb-8 text-lg text-center">{{ props.description }}</p>
-    <a class="btn-primary" :href="props.buttonHref">{{ props.buttonLabel }}</a>
-  </div>
+    <a
+      class="duration-500 btn-primary group-hover:bg-primary-800 group-hover:border-primary-800"
+      :href="props.buttonHref"
+      >{{ props.buttonLabel }}</a
+    >
+  </a>
 </template>
 <script setup lang="ts">
 import { defineProps } from 'vue'
