b10875bdbf131df1956af14f5524b7358803ed71
Ran Cohen <ran@bancor.network>
2021-05-11 15:18:40 +0300

    Removed GMT and first VP path as requested

--- src/gtm/index.ts
+++ src/gtm/index.ts
@@ -2,10 +2,9 @@ export const googleTagManager = () => {
     // @ts-ignore
     if (window.dataLayer) return;
     init(window, document, "script", "dataLayer", "GTM-TCBKR7W");
-    sendGTMPath(undefined, window.location.pathname);
-  };
-  
-  const init = (w: any, d: any, s: any, l: any, i: any) => {
+};
+
+const init = (w: any, d: any, s: any, l: any, i: any) => {
     w[l] = w[l] || [];
     w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
     var f = d.getElementsByTagName(s)[0],
@@ -14,41 +13,4 @@ export const googleTagManager = () => {
     j.async = true;
     j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
     f.parentNode.insertBefore(j, f);
-  };
-  
-  const sendGTM = (data: {}) => {
-    // @ts-ignore
-    const dataLayer = window.dataLayer as {}[];
-    if (dataLayer) dataLayer.push(data);
-  };
-  
-  export const sendGTMEvent = (
-    event: string,
-    ga_event_category: string,
-    event_properties: {} | undefined = undefined
-  ) =>
-    sendGTM({
-      event: "CE " + event,
-      event_properties: event_properties,
-      user_properties: undefined,
-      ga_event: {
-        category: ga_event_category
-      }
-    });
-  
-  export const sendGTMPath = (
-    from: string | undefined,
-    to: string,
-    darkMode: boolean = false
-  ) =>
-    sendGTM({
-      event: "VP " + to,
-      page: {
-        from_path: from,
-        to_path: to,
-        theme: darkMode ? "Dark" : "Light"
-      },
-      user_properties: undefined,
-      ga_event: undefined
-    });
-  
+};
