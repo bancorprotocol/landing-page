7a0faf112e6a8ce4e61ba466a8d7a0576822d250
Jan Langheimer <langheimer@gmail.com>
2021-11-29 14:34:40 +0100

    add link to marketing banner

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="h-full bg-mobile-cubes md:bg-cubes">
-    <div class="relative bg-black h-[80px] flex md:justify-center items-center">
+    <a href="https://try.bancor.network" target="_blank" class="relative bg-black h-[80px] flex md:justify-center items-center">
       <img class="absolute right-0" src="/images/logos/gfx.svg" width="392px" />
       <div class="flex flex-col md:flex-row md:justify-center items-start md:items-center">
         <img src="/images/logos/slogan-mobile.svg" class="ml-[24px] mb-2 md:hidden" />
@@ -11,7 +11,7 @@
         <img src="/images/logos/slogan.svg" class="ml-[24px] hidden md:block" />
       </div>
 
-    </div>
+    </a>
     <div class="main-content flex flex-col items-center justify-around h-screen min-h-[550px] max-h-[750px]">
       <div class="flex justify-between w-full main-content">
         <img src="/images/logos/bancor.svg" />
